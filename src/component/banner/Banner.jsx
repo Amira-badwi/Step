d651b1e2c97c3789dec7553b40de87d29b1f36fd
yassersayed14 <yassersaid476@gmail.com>
2023-02-22 22:45:29 +0200

    course_review_page

---
+++ src/component/banner/Banner.jsx
@@ -0,0 +1,40 @@
+import "./banner.css";
+
+import { products } from "../../data/products";
+import Rating from "../rating/Rating";
+import ProductDescription from "../productdescription/ProductDescription";
+import Header from "../header/Header";
+
+const Banner = () => {
+    return (
+    <div className="single-product">
+        <Header />
+        <div className="product-wrapper">
+            <div className="product-image-wrapper">
+               <img src={products.image} alt=""  />
+            </div>
+            <div className="product-info">
+                <h1 className="product-title">{products.title}</h1>
+                <Rating rating={products.rating} reviews={products.reviews} />
+                <div className="product-price" >{products.price}</div>
+                <div className="product-add-to-cart">
+                <div> Number</div>
+                <input type="number" min="1" max="10" />
+                <button className="add-to-cart-btn">
+                   Enroll in the course
+                </button>
+                </div>
+                </div>
+        </div>
+        <ProductDescription />
+
+
+
+                
+
+
+
+    </div> );
+}
+ 
+export default Banner;
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,23 @@
+import "./header.css"
+
+const Header = () => {
+    return ( <header className="header">
+        <div className="top-header">
+            <div  className="top-header-menu">
+                <i className="bi bi-list"></i>
+            </div>
+            <div className="top-header-logo">
+                
+                
+            </div>
+            <div className="top-header-text">
+            Reviews About Course
+            </div>
+            <div className="top-header-phone">
+               
+                
+            </div>
+        </div>
+    </header>  );
+}
+export default Header;
