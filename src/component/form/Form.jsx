ef49e2ef697b65eacd4b893961c68461064513ef
yassersayed14 <yassersaid476@gmail.com>
2023-03-01 17:00:39 +0200

    adding review

---
+++ src/component/form/Form.jsx
@@ -0,0 +1,29 @@
+// export default function Form() {
+    
+//     return (
+//         <form className='mt-2'>
+//             <div class='mb-3'>
+//   <label htmlFor='name' className='form-label'>put your Name </label>
+//   <input 
+//   type='text' 
+//   name='name'
+//   className='form-control'
+//   placeholder='your name' />
+// </div>
+// <div className='mb-3'>
+//   <label htmlFor='message' className='form-label'>put your comment</label>
+//   <textarea 
+//    className='form-control'
+//    name='message' 
+//    placeholder='your comment'
+//    rows='3'></textarea>
+// </div>
+// <div className='mb-3'>
+//     <button type='submit' className='btn btn-primary mb-3'>
+//         Add 
+//     </button>
+//   </div>
+//         </form>
+
+//     )
+// }
