c48b6c830d2a7d4e5ee8a7aa653f369a01f6c2d4
zeinab-alhwary <zeinabnebeshi20@gmail.com>
2023-03-06 06:21:18 +0200

    adding

--- src/component/context/AuthContext.js
+++ src/component/context/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthContextProvider=({children})=>{
     useEffect(()=>{
       const unsub= onAuthStateChanged(auth,(user)=>{
             setCurrenUser(user)
-           //console.log(user)
+           console.log(user)
         });
         return()=>{
             unsub();
