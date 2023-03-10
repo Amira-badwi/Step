f4a8e197820e603dc3af632dbf73fbb7da39bcbc
Amira-badwi <83235532+Amira-badwi@users.noreply.github.com>
2023-03-10 16:38:03 +0200

    add review to firebase

--- src/component/listitem/ListItem.jsx
+++ src/component/listitem/ListItem.jsx
@@ -6,27 +6,16 @@ export default function ListItem({review}) {
         <li className='list-group-item d-flex justify-content-between align-items-start'>
             <div className="ms-2 me-auto">
 
-                <div className="fw-bold"> {review.name}</div>
+                <div className="fw-bold"> {review.username}</div>
                 <p>{review.message}</p>
                 <p>
                     {
-                        showRating(review.rating)
+                        showRating(review.rate)
                     }
                 </p>
             </div>  
-            {/* <div className="d-flex flex-column align-items-center">
-                    <button className="btn btn-sm btn-danger mb-2">
-                        <i className="bi bi-trash"></i>
-
-                    </button>
-                    <button className="btn btn-sm btn-warning">
-                        <i className="bi bi-pencil"></i>
-
-                    </button>
-
-            </div> */}
-                
-                     
+      
+                               
 
         </li>
 
