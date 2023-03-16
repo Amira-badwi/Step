a7a48d360e3f70f76a45e9976930c5607a3da177
Amira-badwi <83235532+Amira-badwi@users.noreply.github.com>
2023-03-16 13:57:17 +0200

    save books

--- src/component/listitem/ListItem.jsx
+++ src/component/listitem/ListItem.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { showRating } from '../helpers/Helpers'
 
 export default function ListItem({review}) {
-    console.log(review);
     return (
         <li className='list-group-item d-flex justify-content-between align-items-start'>
             <div className="ms-2 me-auto">
--- src/pages/categorypages/BUTTON.js
+++ src/pages/categorypages/BUTTON.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import { stringify } from '@firebase/util';
+import { doc, updateDoc } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../component/context/AuthContext';
+import { db } from '../../firebase';
 
-export const Buttondelete = ({setModal}) => {
+export const Buttondelete = ({setModal ,id ,saved}) => {
+  const currentUse=useContext(AuthContext)
+  const currentValue=currentUse.currentUser;
+  const currentData=currentUse.userData;
+  const  [flag,setflag] =useState(false);
+
+const deletebook =async(idd,booksUser)=>
+{   
+const array=  booksUser.splice((booksUser.indexOf(id)),1)
+const reviewDoc= doc(db,"users",idd);
+const newfield={booksUser:array};
+await updateDoc(reviewDoc ,newfield);
+setflag(false)
+}
+
+  const updateUser= async(idd,booksUser)=>
+  {
+  const reviewDoc= doc(db,"users",idd);
+  const newfield={booksUser:[...booksUser,id]};
+  await updateDoc(reviewDoc ,newfield);
+setflag(true)
+  }
+
   return (
-    <button className='btnStyle w-100 ' 
+   <>
+    <button className='btnStyle w-25 ' 
     onClick={()=>setModal(true)}>open</button>
+   {
+    saved=="true" ||flag==true ? <button className='btnStyle w-50 ' 
+    onClick={()=>deletebook(currentValue.uid,currentData.booksUser)}>delete</button>
+: <button className='btnStyle w-50 ' 
+onClick={()=>updateUser(currentValue.uid,currentData.booksUser)}>save</button>
+
+   }
+   </>
   )
 }
